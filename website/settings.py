b1ed3f4ef317511f7307a347e0c927010dbfc3c2
mohammed1817 <66457245+mohammed1817@users.noreply.github.com>
2020-11-07 08:12:06 +0530

    Update settings.py

--- website/settings.py
+++ website/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'mxf*xtqevs#*ukotk)r=j!h$=79hbdqx530b3@jd&axk07e!ao'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1:8000']
 
 
 # Application definition
@@ -120,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
